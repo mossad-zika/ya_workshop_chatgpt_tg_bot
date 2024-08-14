e513c1b89ab271f082a36a1473e425ba692b4bf6
Evgenii Pavlov <mossad@avride.ai>
2024-08-14 04:28:57 +0300

    filter out EDITED

--- telegram_bot/app/bot.py
+++ telegram_bot/app/bot.py
@@ -302,7 +302,7 @@ def main() -> None:
 
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("image", generate_image))
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, gpt_prompt))
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND & ~filters.UpdateType.EDITED, gpt_prompt))
 
     # Run the bot until the user presses Ctrl-C
     application.run_polling(allowed_updates=Update.ALL_TYPES)
